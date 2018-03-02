854ea24193ab34904c806cf31bc7554bdf19a2ac
akat32 <baduk32127@gmail.com>
2018-03-03 03:59:33 +0900

    finish..?

--- routes/models/apply.js
+++ routes/models/apply.js
@@ -1,13 +1,13 @@
 module.exports = (router, Users)=>{
   router.post('/', async(req,res)=>{
     var new_apply = new Users(req.body);
-    result = await new_apply.save();
+    var result = await new_apply.save();
     if(!result.ok) res.status(200).json({message : "save success!"});
     else res.status(500).json({message : "save failed!"});
   })
   .post('/chk', async (req,res)=>{
-    var result = await Users.find();
-    res.status(200).json(result);
+    var result2 = await Users.find();
+    res.status(200).json(result2);
   })
   return router;
 }
