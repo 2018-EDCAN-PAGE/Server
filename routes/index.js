f3862029eddc8c30f2f7d6abe81e19d6dd0d80f6
akat32 <baduk32127@gmail.com>
2018-03-03 00:15:20 +0900

    update view

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,9 @@
 module.exports = (router,fs,path, Users, multiparty)=>{
-  var portfolio = require('./models/file')(router, Users, fs, path, multiparty);
+  //var portfolio = require('./models/file')(router, Users, fs, path, multiparty);
   var apply = require('./models/apply')(router, Users);
 
-  router.use('/file', portfolio);
+  //router.use('/file', portfolio);
   router.use('/apply', apply);
-  
+
   return router;
 };
