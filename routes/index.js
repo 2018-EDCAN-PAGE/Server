b1a2b9a84f10d3dc3a2a967df61ae9f60e5bd721
iwin2471 <iwin247@edcan.kr>
2018-03-04 22:12:24 +0900

    add finish

--- routes/index.js
+++ routes/index.js
@@ -2,9 +2,10 @@ module.exports = (express, fs,path, Users, multiparty)=>{
   var router = express.Router();
 
   var apply = require('./models/apply')(express.Router(), Users);
-  var finish = require('./models/apply')(express.Router());
+  var finish = require('./models/finish')(express.Router());
 
   router.use('/apply', apply);
+  router.use('/finish', finish);
 
   return router;
 };
